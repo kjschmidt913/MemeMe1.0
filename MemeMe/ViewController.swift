05a6a5730ad136ca3630ad92c964241381d003a3
KJ <kj.schmidt913@gmail.com>
2018-01-12 13:18:28 -0500

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by KJ Schmidt on 1/12/18.
+//  Copyright © 2018 KJ Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
